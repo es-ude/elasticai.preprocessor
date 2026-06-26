3c2710e4332d2bbd1b9cd170a1d043adcdff1541
Florian Hettstedt <florian.hettstedt@uni-due.de>
2026-06-26 11:03:05 +0200

    feat(downsampling): generate C design

---
+++ elasticai/creator_plugins/downsampling/c/downsampling_subsampling_template.h
@@ -0,0 +1,42 @@
+#ifndef DOWNSAMPLING_SUBSAMPLING_TEMPLATE_H
+#define DOWNSAMPLING_SUBSAMPLING_TEMPLATE_H
+
+#include <stdint.h>
+
+
+#ifndef DEF_DOWNSAMPLING_SUBSAMPLING_OUTPUT_LENGTH
+#define DEF_DOWNSAMPLING_SUBSAMPLING_OUTPUT_LENGTH(id, factor) \
+uint32_t get_downsampling_subsampling_output_length_ ## id(uint32_t input_length) { \
+    return (input_length + factor - 1u) / factor; \
+}
+#endif
+
+
+#ifndef DEF_DOWNSAMPLING_SUBSAMPLING_IMPL
+#define DEF_DOWNSAMPLING_SUBSAMPLING_IMPL(id, input_type, factor) \
+void downsample_subsampling_ ## id( \
+    const input_type *input, input_type *output, uint32_t input_length, uint8_t augment \
+) { \
+    const uint32_t output_length = get_downsampling_subsampling_output_length_ ## id(input_length); \
+    const uint32_t offsets = augment ? factor : 1u; \
+    for (uint32_t offset = 0u; offset < offsets; ++offset) { \
+        for (uint32_t output_index = 0u; output_index < output_length; ++output_index) { \
+            const uint32_t input_index = offset + output_index * factor; \
+            output[offset * output_length + output_index] = \
+                input_index < input_length ? input[input_index] : (input_type)0; \
+        } \
+    } \
+}
+#endif
+
+
+#ifndef DEF_DOWNSAMPLING_SUBSAMPLING_PROTO
+#define DEF_DOWNSAMPLING_SUBSAMPLING_PROTO(id, input_type) \
+uint32_t get_downsampling_subsampling_output_length_ ## id(uint32_t input_length); \
+void downsample_subsampling_ ## id( \
+    const input_type *input, input_type *output, uint32_t input_length, uint8_t augment \
+);
+#endif
+
+
+#endif
