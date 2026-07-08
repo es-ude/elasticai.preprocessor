b72ad7dcf2ceb0fd5e3b9e08c22c7cc0bcae0778
Tabea Loeffel <tabea.loeffel@stud.uni-due.de>
2026-07-08 20:02:19 +0200

    commit for branch switch

---
+++ elasticai/creator_plugins/thresholding/src/mov_avg_norm.py
@@ -0,0 +1,39 @@
+from collections.abc import Iterable
+from datetime import datetime
+
+from elasticai.creator.file_generation.resource_utils import read_text
+from elasticai.creator.hdl_ir import DataGraph
+from elasticai.creator.ir2verilog import (
+    Code,
+    Registry,
+    TemplateDirector,
+    type_handler_iterable,
+)
+
+
+@type_handler_iterable()
+def mov_avg_norm(impl: DataGraph, _: Registry) -> Iterable[Code]:
+    package_path = "elasticai.creator_plugins.thresholding"     #change this to current module
+    path2file = "verilog/mov_avg_norm.v"                        #change this to current module
+
+    #Parameters need to fit to verilog module
+    _template = (
+        TemplateDirector()
+        .parameter("BITWIDTH")
+        .parameter("LENGTH")
+        .add_module_name()
+        .set_prototype("\n".join(read_text(package_path, path2file)))
+        .build()
+    )
+    code = list()
+    code.append(
+        (
+            impl.name,
+            _template.substitute(
+                module_name=impl.name.upper(),
+                date_copy_created=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+                **impl.attributes,
+            ),
+        )
+    )
+    return code
