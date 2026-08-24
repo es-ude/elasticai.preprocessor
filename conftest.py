13ffd6acc7d825ffe623e59ddcfafc737668f236
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-08-24 16:00:51 +0200

    feat(conf): Define funcs for starting and after ending tests (delete build_test folder)

--- conftest.py
+++ conftest.py
@@ -1,1 +1,27 @@
 pytest_plugins = ["elasticai.creator.testing"]
+
+
+def pytest_sessionstart(session):
+    from elasticai.preprocessor import get_path_to_project
+    from shutil import rmtree
+
+    path2temp = get_path_to_project("build_test")
+    if path2temp.exists():
+        rmtree(path2temp, ignore_errors=True)
+    path2temp.mkdir(parents=True, exist_ok=True)
+
+
+def pytest_sessionfinish(session, exitstatus):
+    from elasticai.preprocessor import get_path_to_project
+    from pathlib import Path
+
+    path2check = get_path_to_project("build_test")
+    def remove_empty_dirs(path: Path) -> None:
+        if not path.exists():
+            return
+        for dirpath in path2check.iterdir():
+            try:
+                dirpath.rmdir()
+            except:
+                pass
+    remove_empty_dirs(path2check)
