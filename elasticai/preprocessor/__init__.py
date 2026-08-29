c3339a01478b995482988aa923e6e4d48c348b2a
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-08-29 16:54:23 +0200

    feat(sequential): Add first template for PreprocessingSequential and test with dummy structures

--- elasticai/preprocessor/__init__.py
+++ elasticai/preprocessor/__init__.py
@@ -2,3 +2,15 @@ from ._basics import get_path_to_project as get_path_to_project
 from ._logger import define_logger_runtime as define_logger_runtime
 from ._logger import define_logger_runtime_debug as define_logger_runtime_debug
 from ._logger import define_logger_testing as define_logger_testing
+from .sequential import (
+    PreprocessingModule as PreprocessingModule,
+)
+from .sequential import (
+    PreprocessingSequential as PreprocessingSequential,
+)
+from .sequential import (
+    SettingsCreateSequential as SettingsCreateSequential,
+)
+from .sequential import (
+    TargetsBuildPlatform as TargetsBuildPlatform,
+)
