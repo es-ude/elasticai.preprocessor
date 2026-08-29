6b991ec760e06b3568231e7fd5a145d9ead5ad06
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-08-29 21:33:27 +0200

    fix(sda): Refactored using splitted classes

--- elasticai/preprocessor/sda/__init__.py
+++ elasticai/preprocessor/sda/__init__.py
@@ -1,3 +1,4 @@
 from .sda import DefaultSettingsSDA as DefaultSettingsSDA
 from .sda import SettingsSDA as SettingsSDA
 from .sda import SpikeDetection as SpikeDetection
+from .sda import FrameWaveform as FrameWaveform
